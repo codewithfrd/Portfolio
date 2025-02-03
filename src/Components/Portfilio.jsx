a942258600b897c6290dbf65feb17394f52a04f9
codewithfrd <codewithfrd@gmail.com>
2025-02-03 14:57:25 +0530

    add todo list project

--- src/Components/Portfilio.jsx
+++ src/Components/Portfilio.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Portfilio() {
   const cardItem = [
     {
       id: 1,
-      img: "https://blog.zegocloud.com/wp-content/uploads/2024/02/landing-page-frontend-project-1024x576.jpg",
+      img: "https://i.ytimg.com/vi/G0jO8kUrg-I/maxresdefault.jpg",
       name: "fronted",
+      path: "https://radiant-marigold-8db9d8.netlify.app/",
       about:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fugit ea voluptatem tempore.",
     },
@@ -41,18 +43,38 @@ function Portfilio() {
 
   return (
     <>
-      <div name="Portfolio" className="max-w-screen-2xl container mx-auto px-4 md:px-20">
+      <div
+        name="Portfolio"
+        className="max-w-screen-2xl container mx-auto px-4 md:px-20"
+      >
         <div className="">
           <h1 className="text-3xl font-bold my-3">Portfolio</h1>
           <span className="underline font-semibold">Featured Project</span>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-3">
-            {cardItem.map((data)=>{
-                return <div key={data.id} className="shadow-md p-3 lg:pb-10 rounded-2xl overflow-hidden">
-                    <img src={data.img} alt="" className="w-full sm:h-[55%] rounded-t-2xl" />
-                    <h2 className="mt-2 font-bold text-2xl text-blue-600">{data.name}</h2>
-                    <p className="text-gray-500 font-semibold">{data.about}</p>
-                    <button className="mt-2 px-4 py-2 bg-red-400 rounded-md text-white cursor-pointer hover:bg-green-600 duration-200">Show Now</button>
+            {cardItem.map((data) => {
+              return (
+                <div
+                  key={data.id}
+                  className="shadow-md p-3 lg:pb-10 rounded-2xl overflow-hidden"
+                >
+                  <img
+                    src={data.img}
+                    alt=""
+                    className="w-full sm:h-[55%] rounded-t-2xl"
+                  />
+                  <h2 className="mt-2 font-bold text-2xl text-blue-600">
+                    {data.name}
+                  </h2>
+                  <p className="text-gray-500 font-semibold mb-2">{data.about}</p>
+
+                  <a
+                    href={data.path}
+                    className="text-blue-500 font-semibold hover:underline "
+                  >
+                    show now
+                  </a>
                 </div>
+              );
             })}
           </div>
         </div>
